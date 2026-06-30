01d244e8481435a4c198177372c57d801540919c
glacialage12138-collab <glacialage12138@gmail.com>
2026-06-30 10:39:04 +0800

    Upgrade service worker cache and asset versions
    Updated cache name and asset versions in service worker.

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "eat-lite-v3";
+const CACHE_NAME = "eat-lite-v4";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=20260630b",
-  "./script.js?v=20260630b",
+  "./styles.css?v=20260630c",
+  "./script.js?v=20260630c",
   "./manifest.webmanifest",
   "./app-icon.svg"
 ];
@@ -32,32 +32,5 @@ self.addEventListener("fetch", (event) => {
       caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
       return response;
     }).catch(() => caches.match(event.request))
-  );
-});
-const CACHE_NAME = "eat-lite-v2";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./script.js",
-  "./manifest.webmanifest",
-  "./app-icon.svg"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
